9bde1c744578528140a6041b0e932fc4abdfb25b
Przemek Saja <przemek.saja@gmail.com>
2018-07-18 21:02:41 +0200

    Make another change

--- components/Contact.js
+++ components/Contact.js
@@ -5,15 +5,15 @@ var Contact = React.createClass({
 
   render: function() {
     return (
-      <div className={'contactItem'}>
-        <img className={'contactImage'} src={'http://icons.veryicon.com/ico/System/100%20Flat%20Vol.%202/contacts.ico'}/>
-        <p className={'contactLabel'}>
+      <div className='contactItem'>
+        <img className='contactImage' src={'http://icons.veryicon.com/ico/System/100%20Flat%20Vol.%202/contacts.ico'}>
+        <p className='contactLabel'>
           Imię: {this.props.item.firstName}
         </p>
-        <p> className={'contactLabel'}>
+        <p> className='contactLabel'>
           Nazwisko: {this.props.item.lastName}
         </p>
-        <a className={'contactEmail'} href={'mailto:' + this.props.item.email}>
+        <a className='contactEmail' href={'mailto:' + this.props.item.email}>
           {this.props.item.email}
         </a>
       </div>

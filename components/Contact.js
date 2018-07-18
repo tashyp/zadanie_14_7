f7f3769486b4aa72e655ad5b36f01c3ed3f8bd5e
Przemek Saja <przemek.saja@gmail.com>
2018-07-18 20:47:40 +0200

    Fix problem in e-mail section

--- components/Contact.js
+++ components/Contact.js
@@ -13,7 +13,7 @@ var Contact = React.createClass({
         <p> className={'contactLabel'}>
           Nazwisko: {this.props.item.lastName}
         </p>
-        <a href={'mailto:' + this.props.item.email}>
+        <a className={'contactEmail'} href={'mailto:' + this.props.item.email}>
           {this.props.item.email}
         </a>
       </div>

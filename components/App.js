627621916aa2a59ffaa5c0f2c528fb085d1c01e3
tashyp <38553269+tashyp@users.noreply.github.com>
2018-08-03 13:29:50 +0200

    Update App.js

--- components/App.js
+++ components/App.js
@@ -31,7 +31,7 @@ var App = React.createClass({
     return (
       React.createElement('div', {className: 'app'},
         React.createElement(ContactForm, {contact: contactForm}),
-        React.createElement(Contact, {items: contacts}, {})
+        React.createElement(Contacts, {items: contacts}, {})
       )
     );
   }

89f3df4c3f86b8ee97565c8485a3c8a64c8354ed
Przemek Saja <przemek.saja@gmail.com>
2018-08-03 13:02:09 +0200

    Change .js files

--- components/Contact.js
+++ components/Contact.js
@@ -1,22 +1,21 @@
 var Contact = React.createClass({
-  propTypes: {
-    item: React.PropTypes.object.isRequired,
-  },
-
-  render: function() {
-    return (
-      <div className='contactItem'>
-        <img className='contactImage' src={'http://icons.veryicon.com/ico/System/100%20Flat%20Vol.%202/contacts.ico'}>
-        <p className='contactLabel'>
-          Imię: {this.props.item.firstName}
-        </p>
-        <p> className='contactLabel'>
-          Nazwisko: {this.props.item.lastName}
-        </p>
-        <a className='contactEmail' href={'mailto:' + this.props.item.email}>
-          {this.props.item.email}
-        </a>
-      </div>
-    )
-  },
+    propTypes: {
+        item: React.PropTypes.object.isRequired,
+    },
+    render: function () {
+        return (
+            <div className={'contactItem'}>
+                <img className={'contactImage'} src={'http://aktywni.spoldzielnie.org/wp-content/uploads/2014/05/ikona-email.jpg'} />
+                <p className={'contactLabel'}>
+                    Imię: {this.props.item.firstName}
+                </p>
+                <p className={'contactLabel'}>
+                    Nazwisko: {this.props.item.lastName}
+                </p>
+                <a className={'contactEmail'} href={'mailto:' + this.props.item.email}>
+                    {this.props.item.email}
+                </a>
+            </div>
+        )
+    },
 });
--- components/Contacts.js
+++ components/Contacts.js
@@ -1,10 +1,10 @@
-var Contacts = React.createClass({
+var contacts = React.createClass({
   propTypes: {
     items: React.PropTypes.array.isRequired,
   },
 
   render: function() {
-    var contacts = this.props.items.map(function(contact) {
+    var contact = this.props.items.map(function(contact) {
         return React.createElement(Contact, {item: contact, key: contact.id});
     });
 

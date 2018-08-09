b6a826226ea727275a665609b58b84317eda554c
tashyp <38553269+tashyp@users.noreply.github.com>
2018-08-09 18:43:43 +0200

    Fix problems in Contacts.js

--- components/Contacts.js
+++ components/Contacts.js
@@ -1,10 +1,10 @@
-var contacts = React.createClass({
+var Contacts = React.createClass({
   propTypes: {
     items: React.PropTypes.array.isRequired,
   },
 
   render: function() {
-    var contact = this.props.items.map(function(contact) {
+    var contacts = this.props.items.map(function(contact) {
         return React.createElement(Contact, {item: contact, key: contact.id});
     });
 
